1ed3ddde2230c5a23081b94f890b3345696e088c
porameht <porameht@outlook.com>
2024-05-18 22:18:38 +0700

    feat: Add progress.json file and mining-lazada.py script for web scraping Lazada website

---
+++ parameter_noc.py
@@ -0,0 +1,311 @@
+# เฟอร์นิเจอร์ และของแต่งบ้าน
+params1 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["666"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# เครื่องใช้ไฟฟ้า
+params2 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["48999"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# ห้องน้ำ
+params3 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["35358"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# ครัว
+params4 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["51533"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# เครื่องออกกำลังกายและอุปกรณ์
+params5 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["215792"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# พื้นและพรม
+params6 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["474"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# สินค้าที่ใช้นอกบ้าน
+params7 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["475"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# ประตูและหน้าต่าง
+params8 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["473"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# สินค้าโครงสร้าง
+params9 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["88"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# ม่านและมู่ลี่
+params10 = {
+  "lang": "th",
+  "userType": "BUYER",
+  "locale": "th",
+  "orgIdfier": "scg",
+  "f": [
+    {
+      "field": "primaryCategory.o_id",
+      "type": "subcategory2",
+      "codes": ["472"]
+    },
+    {
+      "hidden": False,
+      "field": "stockStatus",
+      "operator": "eq",
+      "codes": ["IN_STOCK", "LOW_STOCK", "isMadeToOrder.code"]
+    }
+  ],
+  "limit": 48,
+  "page": 2,
+  "transformData": True,
+  "searchListMetaInfo": {
+    "nocNocChoiceLimit": 8,
+    "comingSoonLimit": 8,
+    "collapsedView": False
+  },
+  "abType": "A",
+  "relevanceScoreABType": "ranking_score_b"
+}
+
+# โคมไฟและพัดลมเพดาน
